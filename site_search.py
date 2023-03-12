b307d1fc41d2f1b012474eae56e888601da7c61b
khusnutdinov <roman_kh@ngs.ru>
2023-03-12 21:48:38 +0700

    add datalist for region search

--- site_search.py
+++ site_search.py
@@ -1,3 +1,4 @@
+import search_func
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,12 +9,12 @@ def index():
 
 @app.route("/search")
 def search():
-    return render_template('search.html')
+    return render_template('search.html', regions = search_req.areas_dict)
 
 @app.route("/results")
 def results():
     return render_template('results.html')
 
 if __name__ == '__main__':
-
+    search_req = search_func.Search()
     app.run(debug = True)
